9134e16fc3b7803ca3b13b56fed2686ea976abde
ecshenryho <ecshenryho@gmail.com>
2018-08-12 12:11:57 -0700

    strings reviews

---
+++ string.py
@@ -0,0 +1,13 @@
+print('D:\some\name')
+# using raw strings if don't want characters prefaced by \
+print(r'D:\some\name')
+
+
+# string literals can span multiple line using '''...''' or """..."""
+print("""\
+Drink: add ons [OPTIONS]
+
+       1. Rainbow jelly
+       2. Honey boba
+       3. Golden boba
+""")
