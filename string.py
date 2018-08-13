4bb21619a77a2d7e82af1e88411d22846cbe6011
ecshenryho <ecshenryho@gmail.com>
2018-08-13 11:13:41 -0700

    remove spaces

--- string.py
+++ string.py
@@ -2,7 +2,6 @@ print('D:\some\name')
 # using raw strings if don't want characters prefaced by \
 print(r'D:\some\name')
 
-
 # string literals can span multiple line using '''...''' or """..."""
 print("""\
 Drink: add ons [OPTIONS]
@@ -13,28 +12,20 @@ Drink: add ons [OPTIONS]
 """)
 
 # string len
-
 text = "testing len of a string"
-
 print(len(text))
-
 
 # accessing value in strings
 var1 = 'Hello World'
 var2 = 'Python Programing'
-
 print("var1[0]: ", var1[0])
 print("var2[1:5]: ", var2[1:5])
 
-
 # update string
-
 print("updated string: ", var1[:6] + 'Python')
 
 # string format operator
-
 print("My car is %s and it is %d model!" % ('Toyota', 2015))
 
 # unicode string
-
 print(u"Hello,World")
